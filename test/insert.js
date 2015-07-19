568d2ec810cfb0a5acc7dc4da523f95eef63fbb0
Coding <Coding@Jiecaos-MacBook-Pro.local>
2015-07-19 01:36:11 -0400

    still on userDAO, struggling with call back

--- test/insert.js
+++ test/insert.js
@@ -1,17 +1,18 @@
 var mongoose = require('mongoose');
-var database = "mongodb://elio303:ekotani@ds059692.mongolab.com:59692/getitdone";
+var GLOBAL_CONSTANTS = require(__dirname + '/../GLOBAL_CONSTANTS.js');
+var database = require(__dirname + '/../model/database.js');
 
-mongoose.connect(database);
+database.connect();
 
-var db = mongoose.connection;
 var userProfileSchema = new mongoose.Schema({
 	_id : String,
 	password : Number
-}, { collection: 'User', _id: false});
+}, { collection: GLOBAL_CONSTANTS.MODEL.TABLE_NAME.TEST, _id: false});
 
 var UserProfile = mongoose.model("UserProfile", userProfileSchema);
 
-var userJiecao = new UserProfile({_id: 'jiecao.wang@gmail.com', password: 1234})
+var userDAO = {_id: 'jiecao.wang@gmail.com', password: 1234};
+var userJiecao = new UserProfile(userDAO);
 
 
 userJiecao.save(function (err, userJiecao) {
