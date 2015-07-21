2cd4b7afc71e266b55b15bf9e0a18bd934389355
Coding <Coding@Jiecaos-MacBook-Pro.local>
2015-07-20 23:23:50 -0400

    fixed the index.js

--- index.js
+++ index.js
@@ -13,20 +13,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
 	//we will put this into route.js once this is working
-	if (request.body.inputEmail && request.body.inputPassword){
-		var userId = request.body.inputEmail;
-		var userPwd = request.body.inputPassword;
-		UserDAO.validate(userId, userPwd, 
-			function(v){
-				if (v){
-					response.send('yes, you are an exting user');
-				} else {
-					response.send('No, you dont exist');
-				}
-			});
-	} else {
-		response.sendFile(path.join(__dirname + '/index.html'));
-	}
+	response.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Should define a regex here so that we can send whenever asked for user collection
@@ -38,9 +25,17 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.post('/', function(req, res){
-  var userName = req.body.inputEmail;
-  var html = 'Hello: ' + userName + '.<br>' +
-             '<a href="/">Try again.</a>';
-  res.send(html);
+app.post('/', function(request, response){
+	if (request.body.inputEmail && request.body.inputPassword){
+	var userId = request.body.inputEmail;
+	var userPwd = request.body.inputPassword;
+	UserDAO.validate(userId, userPwd, 
+		function(v){
+			if (v){
+				response.send('yes, you are an exting user');
+			} else {
+				response.send('No, you dont exist');
+			}
+		});
+} 
 });
