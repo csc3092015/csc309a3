044f701d788e3af61ba0f70cb57d3b701556a55f
Coding <Coding@Jiecaos-MacBook-Pro.local>
2015-07-20 23:08:57 -0400

    trying user authentication

---
+++ control/bussinessObject/UserBO.js
@@ -0,0 +1,7 @@
+ /*******************************Dummy Constructor**************************************/ function User (userId, password, userName, userIdType){
+function User (userId, password, userName, userIdType){
+	this._userId = userId;
+	this._password = password;
+}
+
+module.exports = User;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express(); 
+var UserDAO = require('./model/dao/UserDAO.js');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,7 +12,21 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname + '/index.html'));
+	//we will put this into route.js once this is working
+	if (request.body.inputEmail && request.body.inputPassword){
+		var userId = request.body.inputEmail;
+		var userPwd = request.body.inputPassword;
+		UserDAO.validate(userId, userPwd, 
+			function(v){
+				if (v){
+					response.send('yes, you are an exting user');
+				} else {
+					response.send('No, you dont exist');
+				}
+			});
+	} else {
+		response.sendFile(path.join(__dirname + '/index.html'));
+	}
 });
 
 // Should define a regex here so that we can send whenever asked for user collection
@@ -23,4 +38,9 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+app.post('/', function(req, res){
+  var userName = req.body.inputEmail;
+  var html = 'Hello: ' + userName + '.<br>' +
+             '<a href="/">Try again.</a>';
+  res.send(html);
+});
