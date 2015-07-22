2cec43ed5bb851d5ad8fdaeb64d91f47100f1cfd
sandraling <s.sandra.ling@gmail.com>
2015-07-22 04:00:35 -0400

    Added node_modules

---
+++ GLOBAL_CONSTANTS.js
@@ -0,0 +1,19 @@
+/***************************************MODEL CONSTANTS**********************************************/
+var model = {};
+model.DATABASE_URI = "mongodb://elio303:ekotani@ds059692.mongolab.com:59692/getitdone";
+
+	//TABLE NAME
+model.TABLE_NAME = {};
+model.TABLE_NAME.USER = "User";
+model.TABLE_NAME.TEST = "Test";
+
+
+/***************************************CONTROL CONSTANTS**********************************************/
+// module.exports.CONTROL = {};
+
+
+/***************************************VIEW CONSTANTS**********************************************/
+// module.exports.VIEW = {};
+
+
+module.exports.MODEL = model;
